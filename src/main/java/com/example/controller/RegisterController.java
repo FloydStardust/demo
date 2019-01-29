e5e07090cb8d2e9aee93b2208316535b9869d2ec
xxh13 <1095279176893@tianchi.com>
2019-01-29 19:23:24 +0800

    register 页面

---
+++ src/main/java/com/example/controller/RegisterController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: xxh
+ * Date: 2019/1/29
+ * Time: 6:43 PM
+ */
+
+@Controller
+public class RegisterController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "register")
+	public String register(ModelMap model) {
+		return "signup";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "login")
+	public String login(ModelMap model) {
+		return "login";
+	}
+
+}
